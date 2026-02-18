cf22151274e982a958d18a4b70703991361fee72
kundwa pacis1 <gihozokundwa250@gmail.com>
2026-02-18 23:57:52 +0200

    Update README

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,7 @@
 export default function Settings() {
     return (
         <div className="p-6">
-            <h1 className="text-2xl font-bold">Settings Page</h1>
+            <h1 className="text-2xl font-bold">Settings Pages</h1>
         </div>
     );
 }
